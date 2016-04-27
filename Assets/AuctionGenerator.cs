2fb80bed493169d26fd7cf9fefe42314b2673ddb
phil <philipp.zupke@wooga.net>
2016-04-27 10:44:40 +0200

    initial commit

---
+++ Assets/AuctionGenerator.cs
@@ -0,0 +1,235 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Used for weighted lookup of "Positions"
+/// </summary>
+[System.Serializable]
+public class PositionWeight{
+	public Positions position;
+	public float weight;
+}
+
+/// <summary>
+/// Rule for the occurence and the min and max value, used for "Age" and "Level"
+/// </summary>
+[System.Serializable]
+public class AuctionGenerationRule{
+	public int count;
+	public float min;
+	public float max;
+}
+
+
+public enum Positions{
+	GK, STR, CB, LW, LB, DMC, AMC, RB, RW, MC
+}
+
+
+public class Auction  {
+
+	public Positions Position;
+	public int Age;
+	public float Level;
+
+	public Auction(Positions _pos, float _level, float _age){
+		Age = (int) Mathf.Round(_age);
+		Level = _level;
+		Position = _pos;
+	}
+
+	public override string ToString ()
+	{
+		return Position.ToString()+" | lvl "+Level+" | age "+Age;
+	}
+}
+
+
+public class AuctionGenerator : MonoBehaviour {
+
+	//LevelConfigs + Age Configs MUST BE OF THE SAME LENGTH
+	public List<AuctionGenerationRule> PlayerLevelConfigs;
+	public List<AuctionGenerationRule> PlayerAgeConfigs;
+
+	//PositionImportance and Startweights MUST BE IN SAME ORDER
+	public List<PositionWeight> PositionImportance;
+	public List<PositionWeight> PositionStartWeights;
+
+	//These buckets will be filled 
+	List<Positions> position_bucket;
+	List<float> age_bucket;
+	List<float> level_bucket;
+
+
+	void Start(){
+		GenerateMarket();
+	}
+
+
+	public void GenerateMarket(){
+
+		DeleteUI();
+
+		//fill the age and level buckets
+		level_bucket = CreateNumericBucket(PlayerLevelConfigs);
+		age_bucket = CreateNumericBucket(PlayerAgeConfigs);
+
+		//fill the position bucket
+		position_bucket = CreatePositionBucket(PositionStartWeights, PositionImportance, age_bucket.Count);
+
+		//use the buckets to create auctions
+		List<Auction> auctions = GenerateAuctionsFromBuckets(age_bucket, level_bucket, position_bucket);
+
+		foreach(Auction auction in auctions){
+			Debug.Log(auction.ToString());
+		}
+
+		GenerateUIList(auctions);
+	}
+
+
+	List<float> CreateNumericBucket(List<AuctionGenerationRule> rules){
+		
+		List<float> bucket = new List<float>();
+
+		foreach(AuctionGenerationRule rule in rules){
+			for(int i = 0; i < rule.count; i++){
+				bucket.Add( Random.Range(rule.min, rule.max));
+			}
+		}
+
+		return bucket;
+	}
+
+	/// <summary>
+	/// 1. Creates a bucket with positions
+	/// 2. To determine a position we use a weighted list of positions
+	/// 3. The weights of this list will change in every iteration
+	/// 3.1. The weight of the choosen position will be set to 0
+	/// 3.2. The weight of ALL OTHER positions will be increased by a "static" value, defined in "PositionImportance"
+	/// 3.3. -> For each iteration over the "weighted positions" the weights are altered
+	/// </summary>
+	/// <returns>The weighted position.</returns>
+	/// <param name="positions">Positions.</param>
+	List<Positions> CreatePositionBucket(List<PositionWeight> start_weights, List<PositionWeight> importance, int count){
+		
+		List<Positions> bucket = new List<Positions>();
+
+		//we use the already configured list of start weights for the first iteration, and then continue iterating with that list, it will be modified
+		List<PositionWeight> current_weights = new List<PositionWeight>(start_weights);
+
+		for(int i = 0; i < count;i++){
+			//find a position from the weighted list
+			Positions pos = GetWeightedPosition(current_weights);
+
+			//update the weighted list (chosen position will be set to 0, all the others will be increased)
+			UpdateWeights(current_weights,importance, pos);
+
+			bucket.Add(pos);
+		}
+
+		return bucket;
+
+	}
+
+
+	/// <summary>
+	/// Updates the weights.
+	/// </summary>
+	/// <param name="current">Current.</param>
+	/// <param name="importance">Importance.</param>
+	/// <param name="reset_pos">Reset position.</param>
+	void UpdateWeights(List<PositionWeight> current, List<PositionWeight> importance, Positions reset_pos){
+		Debug.Log("updating weights pos:"+ reset_pos);
+
+		for(int i = 0; i < current.Count; i++){
+			if(current[i].position == reset_pos){
+				current[i].weight = 0;
+			} else{
+				current[i].weight += importance[i].weight;
+			}
+		}
+	}
+
+
+	List<Auction> GenerateAuctionsFromBuckets(List<float> ages, List<float> levels, List<Positions> positions){
+		
+		//All lists must be of the same length, if not, configuration has been messed up
+		if(ages.Count != levels.Count || levels.Count != positions.Count) {
+			Debug.LogWarning("different bucket sizes age"+ages.Count+" levels "+levels.Count+" pos "+positions.Count);
+			return null;
+		}
+
+		List<Auction> auctions = new List<Auction>();
+
+		//for now, the number of auctions is simply the length of one of the lists (since they are all the same length)
+		int auction_count = ages.Count;
+
+		//in each iteration one item will be returned from the bucket (this also removes the item from the bucket)
+		for(int i = 0; i<auction_count;i++){
+			auctions.Add(
+				new Auction( 
+					GetRandomItemFromList(positions),
+					GetRandomItemFromList(levels), 
+					GetRandomItemFromList(ages)
+				)
+			);
+		}
+
+		return auctions;
+
+	}
+
+	//returns random item from a list and removes it
+	static T GetRandomItemFromList<T> (List<T> items){
+		T item = items[Random.Range(0, items.Count)];
+		items.Remove(item);
+		return item;
+	}
+
+
+	Positions GetWeightedPosition(List<PositionWeight> positions){
+		
+		float total_chance = 0;
+
+		//calculate sum of weights
+		foreach(PositionWeight weighted_item in positions){
+			total_chance+= weighted_item.weight;
+		}
+
+		//determine random value for sum of weights
+		float r = Random.value * total_chance;
+		float last = 0;
+
+		//get the item for the r value
+		for(int i = 0; i < positions.Count; i++){
+			if(r > last && r < last + positions[i].weight){
+				return positions[i].position;
+			}
+
+			last+= positions[i].weight;
+		}
+
+		//oh oh
+		Debug.LogWarning("no position from weightable list found!! :(");
+		return Positions.GK;
+	}
+
+
+	void GenerateUIList(List<Auction> auctions){
+
+		foreach(Auction auction in auctions){
+			GameObject obj = Instantiate( Resources.Load("auction_item") )as GameObject;
+			obj.transform.SetParent(transform, false);
+			obj.GetComponent<AuctionUI>().SetAuction(auction);
+		}
+	}
+
+	void DeleteUI(){
+		var Children = new List<GameObject>();
+		foreach(Transform child in transform) Children.Add(child.gameObject);
+		Children.ForEach(child => Destroy(child));
+	}
+
+}
---
+++ Assets/AuctionUI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class PosColor{
+	public Positions pos;
+	public Color col;
+}
+public class AuctionUI : MonoBehaviour {
+
+	public Image PosBG;
+	public Text PosTF;
+	public Image LevelBG;
+	public Text LevelTF;
+
+	public Text AgeTF;
+
+	public List<Color> qualities;
+
+	public List<PosColor> colors;
+
+	public void SetAuction(Auction auction){
+		PosBG.color = GetColForPos(auction.Position);
+		LevelBG.color = GetColForLevel(auction.Level);
+
+		PosTF.text = auction.Position.ToString();
+		LevelTF.text = auction.Level.ToString("F1");
+
+		AgeTF.text = auction.Age.ToString();
+	}
+
+	Color GetColForPos(Positions pos){
+		foreach(PosColor config in colors){
+			if( config.pos == pos) return config.col;
+		}
+
+		return Color.green;
+	}
+
+	Color GetColForLevel(float l){
+		if(l < 6) return qualities[0];
+		if(l < 9) return qualities[1];
+
+		return qualities[2];
+	}
+}
